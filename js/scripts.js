4dfdcc06adcd681a9d1fd9be04d0ee1835f62037
Roman <noovaroman@gmail.com>
2019-06-15 20:05:15 +0200

    Минификация кода

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,4 @@ var popup = document.querySelector(".search-form");
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-show");
-    console.log("Holla");
 });
