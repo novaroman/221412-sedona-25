40555a8e442b1104214346cf600624a92910dd0d
Roman <noovaroman@gmail.com>
2019-06-15 19:51:36 +0200

    Оживление попапа

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Показ и вскрытие попапа
+
+var link = document.querySelector(".search-hotel-button");
+var popup = document.querySelector(".search-form");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
+    console.log("Holla");
+});
