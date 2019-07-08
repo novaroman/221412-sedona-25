0387d7a3a06d71cf0dfd8df0bc8366eee54e7623
Roman <noovaroman@gmail.com>
2019-07-08 11:56:53 +0200

    Исправление ошибок, подготовка к тре́тьей защите проекта

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,23 @@ var popup = document.querySelector(".search-form");
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-show");
+    form.classList.remove("modal-error");
+});
+
+// Состояние ошибки отправки формы
+
+var form = document.querySelector(".search-form-wrapper");
+
+var arrival = popup.querySelector("[name=arrival]");
+var departure = popup.querySelector("[name=departure]");
+var calcAdults = popup.querySelector("[name=adults]");
+var calcChildren = popup.querySelector("[name=children]");
+
+popup.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !calcAdults.value || !calcChildren.value) {
+    evt.preventDefault();
+    form.classList.remove("modal-error");
+    form.offsetWidth = popup.offsetWidth;
+    form.classList.add("modal-error");
+  }
 });
